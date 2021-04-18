280512a313a7235b48950ef952b0bd030f155ab3
AhmedRoshdy <ahmedroshdy01029@gmail.com>
2021-04-19 01:57:18 +0200

    added

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
 
     $('.menu').click(function(){
         $('.navbar').toggle();
-        $('.menu .fa-bars').toggleClass('fa-times');
+       $('.menu .fa-bars').toggleClass('fa-times');
     });
 });
